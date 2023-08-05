1f725fc1cb58879f6a86fe194289b66bea4f0b1c
aditya <addie4120@gmail.com>
2023-08-05 15:56:56 +0530

    connected backend to frontend

--- backend.py
+++ backend.py
@@ -3,20 +3,16 @@ import requests
 API_KEY = "06e6c7afc2f4dc2cd5a0ae622d3884a6"
 
 
-def get_data(place, days=None, option=None):
+def get_data(place, days):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     nr_values = 8 * days
     filtered_data = filtered_data[0:nr_values]
-    if option == "Temperature":
-        filtered_data = [fd["main"]["temp"] for fd in filtered_data]
-    if option == "Sky":
-        filtered_data = [fd["weather"][0]["main"] for fd in filtered_data]
     return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="London", days=2, option="Temperature"))
+    print(get_data(place="London", days=2))
 
--- main.py
+++ main.py
@@ -3,13 +3,30 @@ import plotly.express as px
 from backend import get_data
 
 st.title("WEATHER FORECASTER")
-place = st.text_input("Enter Place")
+city = st.text_input("Enter Place")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of days through this slider")
-options = st.selectbox(options=("temperature", "sky"), label="options")
-st.subheader(f"{options} for the next {days} days in {place}")
+options = st.selectbox(options=("Temperature", "Sky"), label="options")
+st.subheader(f"{options} for the next {days} days in {city}")
 
-df = get_data(place, days, options)
+if city:
+    try:
+        filtered_data = get_data(city, days)
 
-figure = px.line(x=days, y=options, labels={"x": "date", "y": "temperature (c)"})
-st.plotly_chart(figure)
+        if options == "Temperature":
+            temp = [fd["main"]["temp"] for fd in filtered_data]
+            temp = [(((i - 273.115) * 9/5 + 32) - 32) * 5/9 for i in temp]
+            dates = [fd["dt_txt"] for fd in filtered_data]
+            figure = px.line(x=dates, y=temp, labels={"x": "date", "y": "temperature (c)"})
+            st.plotly_chart(figure)
+
+        if options == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [fd["weather"][0]["main"] for fd in filtered_data]
+            image_path = [images[condition] for condition in sky_conditions]
+            st.image(image_path, width=110)
+    except KeyError:
+        st.info("OOPS! THIS PLACE DOES NOT EXIST. PLEASE ENTER A VALID CITY NAME")
+
+
